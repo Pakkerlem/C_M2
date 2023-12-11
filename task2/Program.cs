615555803a82ae31290138fbd8118e24fb8846c2
Pakker <gfabinam@gmail.com>
2023-12-11 03:09:50 +0300

    2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив. Напишите
+// программу, которая поменяет местами первую и
+// последнюю строку массива.
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] array = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = rand.Next(min, max);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangingLines(int[,] arr)
+{
+    int[] tempArray = new int[arr.GetLength(1)];
+
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        tempArray[i] = arr[0, i];
+    }
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        arr[0, i] = arr[arr.GetLength(0) - 1, i];
+    }
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        arr[arr.GetLength(0) - 1, i] = tempArray[i];
+    }
+}
+
+int[,] newArray = GetArray(4, 4, 1, 10);
+PrintArray(newArray);
+ChangingLines(newArray);
+Console.WriteLine();
+PrintArray(newArray);
